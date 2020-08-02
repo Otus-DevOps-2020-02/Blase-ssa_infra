da65991f317ca3a1b41811fab5563b3e8a30cfdf
Sergey Silantev <scorpioncore@gmail.com>
2020-08-02 21:18:08 +0300

    initial hw commit

--- packer/scripts/deploy.sh
+++ packer/scripts/deploy.sh
@@ -3,5 +3,5 @@ useradd -m -r -s /bin/bash puma
 cd ~puma/
 sudo -u puma git clone -b monolith https://github.com/express42/reddit.git
 cd ~puma/reddit/
-bundler install
+sudo bundler install
 #sudo -u puma puma -d
